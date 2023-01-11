de5efebf185313559e8e45b632231d91d138d496
prince475 <danielmwai475@gmail.com>
2023-01-11 08:54:38 +0300

    Adding a title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Article from "./Article";
+import Title from "./Title"
 import Comment from "./Comment";
 
 function App() {
   return (
     <div>
       <Article />
+      <Title/>
       <Comment />
     </div>
   );
---
+++ src/Title.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Title = () => {
+    return<h1>First Assignment</h1>
+}
+export default Title;
